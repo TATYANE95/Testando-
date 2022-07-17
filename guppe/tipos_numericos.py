bc0fbd4951edfbcd72d33148e81504fd57cb6508
TATYANE95 <tatianefreire1@outlook.com>
2022-07-16 22:06:20 -0300

    https://github.com/TATYANE95/Estudos-Python.git
    Signed-off-by: TATYANE95 <TATIANEFREIRE1@OUTLOOK.COM>

---
+++ guppe/tipos_numericos.py
@@ -0,0 +1,105 @@
+"""
+Tipo Numérico
+"""
+# num = 45
+# print(num)
+
+
+# num = {5 + 4}
+# print(num)
+
+
+# num = {4-8}
+# print(num)
+
+
+#num = {5 * 4}
+#print(num)
+
+
+#num = {5/2}
+#print(num)
+
+
+#num = {5//2} Forma correta
+#print(num)
+
+
+#num =int(5/2) Forma errada
+#print(num)
+
+
+#num = {5%2} Saber se um numero é impar ou par o resto da divisão do impar sempre será 1 e do par número 2
+#print(num)
+
+
+#num = {5**2} Número elevado
+#print(num)
+
+
+# Quantos digítos o Python suporta? iniciando pelo Java
+
+
+#num = 2**63
+#print(num)
+
+
+#num = 2**1000000
+#print(num)
+
+
+#Teclas CTRL + l para limpar o console
+
+
+# Identificar um numero muito grande com underline Ex:
+
+
+#num = 1_000_000
+#print(num)
+
+
+#num = 42
+#print(num + 1)
+
+
+#num = num + 1
+#print(num)
+
+
+#num = 23
+#num -= 1
+#print(num)
+
+
+#num = 23
+#num *= 6
+#print(num)
+
+
+#num = 23
+#num = num * 6
+#print(num)
+
+
+#num = 23
+#num /= 8
+#print(num)
+
+
+#num = 23
+#num = num / 8
+#print(num)
+
+
+num = 23
+num //= 2
+print(num)
+
+
+#num = 45
+#type(42) ou type(num) no terminal retorna o tipo de dado que estamos trabalhando.
+
+
+# num = 42
+# dir(num) no terminal retorna quais são as operações podemos realizar com num.
+# Ex: num.__add__(8) acrescentará 8 em num.
